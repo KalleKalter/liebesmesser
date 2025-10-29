5aede9a158b8c44fa9a2fbc5bb5c0eb36cb5587a
KalleKalter <klaaskattner@gmail.com>
2025-10-29 17:24:29 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 basic.forever(function () {
+    basic.setLedColor(basic.rgb(255, 0, 0))
     if (input.pinIsPressed(TouchPin.P0) || input.pinIsPressed(TouchPin.P3)) {
+        let zahl = 0
         basic.showNumber(randint(0, 100))
+        if (zahl >= 65 && zahl <= 100) {
+            basic.showIcon(IconNames.Heart)
+            basic.clearScreen()
+        }
     }
 })
